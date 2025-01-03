2fd1e1c98b4f0ae14fd04bb59c267e3000fed2a1
JoaoPauloHJ <joaopaulohj126@hotmail.com>
2025-01-03 08:46:39 -0300

    Att 0.7 adicionando pop-up

--- js/app.js
+++ js/app.js
@@ -44,3 +44,13 @@ function pagar() {
 
     alert(pagamento);
 }
+
+function openPopup() {
+    document.getElementById('popup').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+  }
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
